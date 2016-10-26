4b666126feb8ad45621b643d3d8673e17cfd1ef6
Michauxquan <79365450@qq.com>
2016-10-26 17:06:26 +0800

    [F] 4.5

---
+++ PCDD.Entity/Common/LogEntity.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks; 
+
+namespace PCDDEntity
+{
+    public class LogEntity
+    {
+        [Property("Lower")]
+        public int AutoID { get; set; }
+
+        public string Remark { get; set; }
+
+        public DateTime CreateTime { get; set; }
+
+        public string UserID { get; set; } 
+
+        public string UserName { get; set; }
+
+        public string LeveID { get; set; }
+
+        public string SeeID { get; set; }
+
+        public string SeeAvatar { get; set; }
+
+        public string SeeName { get; set; }
+
+        public int Type { get; set; }
+
+        public string OpearteID { get; set; }
+
+        public void FillData(System.Data.DataRow dr)
+        {
+            dr.FillData(this);
+        }
+    }
+}
---
+++ PCDD/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PCDD.MvcApplication" Language="C#" %>
